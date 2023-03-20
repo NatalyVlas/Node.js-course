863959ba4359701c92e774e0f4eb43a6521d6f75
Nataly Vlasyuk <kovalevskaya.nataly@gmail.com>
2023-03-20 13:24:36 +0500

    Итоговая работа

--- 3-lesson/6-lesson/server.js
+++ 3-lesson/6-lesson/server.js
@@ -16,13 +16,25 @@ const server = http.createServer((request, response) => {
 });
 
 const io = new Server(server);
+let count = 0;
 
 io.on('connection', (client) => {
-    console.log(`Websocket connected ${client.id}`, client.handshake);
+    console.log(`Websocket connected ${client.id}`);
+    count++;
+    console.log(`count = ${count}`);
+    client.emit('server-con', { count, id: client.id })
+    client.broadcast.emit('server-con', { count, id: client.id })
+
+    client.on('disconnect', () => {
+        count--;
+        console.log(`Websocket disconnected ${client.id}`);
+        console.log(`count = ${count}`);
+        client.broadcast.emit('server-discon', { count: count, id_dis: client.id })
+    })
 
     client.on('client-msg', (data) => {
-        client.broadcast.emit('server-msg', { msg: data.msg });
-        client.emit('server-msg', { msg: data.msg })
+        client.broadcast.emit('server-msg', { msg: data.msg, id: client.id });
+        client.emit('server-msg', { msg: data.msg, id: client.id })
     })
 
 });
